a8958791970c182c314178f6cedf6b39dd4af334
BaabaYaaga <86200239+jhaaj08@users.noreply.github.com>
2024-11-16 14:49:53 +0530

    session-4 auth token created

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, jsonify
 from flask_security import auth_required, current_user, roles_required, SQLAlchemyUserDatastore
-from flask_security.utils import hash_password
+from flask_security.utils import hash_password, verify_password
 from extensions import db
 
 def create_view(app, user_datastore : SQLAlchemyUserDatastore):
@@ -8,6 +8,26 @@ def create_view(app, user_datastore : SQLAlchemyUserDatastore):
     @app.route('/')
     def home():
         return render_template('index.html')
+    
+    @app.route('/user-login', methods=['POST'])
+    def user_login():
+        data = request.get_json()
+        email = data.get('email')
+        password = data.get('password')
+
+        if not email or not password:
+            return jsonify({'message' : 'email or password not provided'}), 400
+        
+        user = user_datastore.find_user(email = email)
+
+        if not user:
+            return jsonify({'message' : 'invalid user'}), 400
+        
+        if verify_password(password, user.password):
+            return jsonify({'token' : user.get_auth_token(), 'user' : user.email, 'role' : user.roles[0].name}), 200
+        else :
+            return jsonify({'message' : 'invalid password'}), 400
+
     
     @app.route('/register', methods=['POST'])
     def register():
@@ -33,3 +53,4 @@ def create_view(app, user_datastore : SQLAlchemyUserDatastore):
             db.session.rollback()
             return jsonify({'message':'error in user creation'}), 408
         return jsonify({'message':'user_created'}),200
+    
